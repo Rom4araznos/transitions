492c2627ec22f07c4182b9f92e2a11fb69f2c250
Rom4araznos <mandrusov2005@mail.ru>
2024-06-30 17:53:56 +0300

    add comments

--- src/transitions.cpp
+++ src/transitions.cpp
@@ -6,7 +6,7 @@
 namespace lx {
 
 
-    auto cubic_bezier(f32 t, vec2f p0, vec2f p1, vec2f p2, vec2f p3) -> vec2f {
+    auto cubic_bezier(f32 t, vec2f p0, vec2f p1, vec2f p2, vec2f p3) -> f32 {
 
         f32 u = 1 - t;
         f32 tt = t * t;
@@ -15,17 +15,25 @@ namespace lx {
         f32 ttt = tt * t;
 
         vec2f p;
-        p.x = uuu * p0.x;         // (1 - t)^3 * p0
-        p.x += 3 * uu * t * p1.x; // 3 * (1 - t)^2 * t * p1
-        p.x += 3 * u * tt * p2.x; // 3 * (1 - t) * t^2 * p2
-        p.x += ttt * p3.x;        // t^3 * p3
+
+        // (1 - t)^3 * p0
+        p.x = uuu * p0.x;
+
+        // 3 * (1 - t)^2 * t * p1
+        p.x += 3 * uu * t * p1.x;
+
+        // 3 * (1 - t) * t^2 * p2
+        p.x += 3 * u * tt * p2.x;
+
+        // t^3 * p3
+        p.x += ttt * p3.x;
 
         p.y = uuu * p0.y;
         p.y += 3 * uu * t * p1.y;
         p.y += 3 * u * tt * p2.y;
         p.y += ttt * p3.y;
 
-        return p;
+        return p.y;
     }
 
     auto linear(u16 max, u16 current) -> f32 {
@@ -35,35 +43,35 @@ namespace lx {
 
     auto ease(u16 max, u16 current) -> f32 {
 
-        vec2f res = cubic_bezier(f32(current) / max, {0.0, 0.0}, {0.25, 0.1},
-                                 {0.25, 1.0}, {1.0, 1.0});
+        f32 res = cubic_bezier(f32(current) / max, {0.0, 0.0}, {0.25, 0.1},
+                               {0.25, 1.0}, {1.0, 1.0});
 
 
-        return res.y;
+        return res;
     }
 
     auto ease_in(u16 max, u16 current) -> f32 {
 
-        vec2f res = cubic_bezier(f32(current) / max, {0.0, 0.0}, {0.42, 0.0},
-                                 {1.0, 1.0}, {1.0, 1.0});
+        f32 res = cubic_bezier(f32(current) / max, {0.0, 0.0}, {0.42, 0.0},
+                               {1.0, 1.0}, {1.0, 1.0});
 
-        return res.y;
+        return res;
     }
 
     auto ease_out(u16 max, u16 current) -> f32 {
 
-        vec2f res = cubic_bezier(f32(current) / max, {0.0, 0.0}, {0.0, 0.0},
-                                 {0.58, 1.0}, {1.0, 1.0});
+        f32 res = cubic_bezier(f32(current) / max, {0.0, 0.0}, {0.0, 0.0},
+                               {0.58, 1.0}, {1.0, 1.0});
 
-        return res.y;
+        return res;
     }
 
     auto ease_in_out(u16 max, u16 current) -> f32 {
 
-        vec2f res = cubic_bezier(f32(current) / max, {0.0, 0.0}, {0.42, 0.0},
-                                 {0.58, 1.0}, {1.0, 1.0});
+        f32 res = cubic_bezier(f32(current) / max, {0.0, 0.0}, {0.42, 0.0},
+                               {0.58, 1.0}, {1.0, 1.0});
 
-        return res.y;
+        return res;
     }
 
 } // namespace lx
--- src/transitions.hpp
+++ src/transitions.hpp
@@ -1,14 +1,72 @@
 #pragma once
 
 #include "number.hpp"
+#include "utils.hpp"
 
 namespace lx {
-
+    /**
+     * @brief Calculate state of ease transition
+     *
+     * @param max Max number of milliseconds
+     * @param current Time passed from the beginning of the transition (in
+     * milliseconds)
+     * @return f32 Percentage of transition progression. Value returned is in
+     * interval [0 ; 1]
+     */
     auto ease(u16 max, u16 current) -> f32;
+    /**
+     * @brief Calculate state of linear transition
+     *
+     * @param max Max number of milliseconds
+     * @param current Time passed from the beginning of the transition (in
+     * milliseconds)
+     * @return f32 Percentage of transition progression. Value returned is in
+     * interval [0 ; 1]
+     */
     auto linear(u16 max, u16 current) -> f32;
+    /**
+     * @brief Calculate state of ease-in transition
+     *
+     * @param max Max number of milliseconds
+     * @param current Time passed from the beginning of the transition (in
+     * milliseconds)
+     * @return f32 Percentage of transition progression. Value returned is in
+     * interval [0 ; 1]
+     */
     auto ease_in(u16 max, u16 current) -> f32;
+    /**
+     * @brief Calculate state of ease-out transition
+     *
+     * @param max Max number of milliseconds
+     * @param current Time passed from the beginning of the transition (in
+     * milliseconds)
+     * @return f32 Percentage of transition progression. Value returned is in
+     * interval [0 ; 1]
+     */
     auto ease_out(u16 max, u16 current) -> f32;
+    /**
+     * @brief Calculate state of ease-in-out transition
+     *
+     * @param max Max number of milliseconds
+     * @param current Time passed from the beginning of the transition (in
+     * milliseconds)
+     * @return f32 Percentage of transition progression. Value returned is in
+     * interval [0 ; 1]
+     */
     auto ease_in_out(u16 max, u16 current) -> f32;
-    auto cubic_bezier(u16 max, u16 current, f32 y1, f32 y2) -> f32;
+    /**
+     * @brief Calculate progression of specified cubic bezier function based on
+     * time.
+     * @param t Time in percentage. Passed value is in
+     * interval [0 ; 1]
+     * @param p0 First control point. The beginning of the function in {x = 0 ,y
+     * = 0}
+     * @param p1 Second interpolated point.
+     * @param p2 Third interpolated point.
+     * @param p3 Last control point. The end of the function in {x = 1 ,y =
+     * 1}
+     * @return f32 Progression of transition.
+     */
+    auto cubic_bezier(f32 t, vec2f p0, vec2f p1, vec2f p2, vec2f p3) -> f32;
 
 }; // namespace lx
